853bb3bddfaa050fd3b5207a4c835d61f2659e39
Christophe Frézier <frezier@gmail.com>
2024-07-19 12:16:43 +0200

    Update ws.ts

--- browser/common/ws.ts
+++ browser/common/ws.ts
@@ -4,7 +4,9 @@
  */
 
 export const createWs = () => {
-    return new WebSocket(window.location.toString()
-        .replace("https://", "wss://wss.")
+    return new WebSocket(
+        window.location.toString()
+            .replace("http://", "ws://").replace(location.port, CONFIG.WSS_PORT + '')
+            .replace("https://", "wss://wss.")
     );
 }
